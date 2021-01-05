cdb21486f85198693edf32ee54c9ae5e8697bf27
Trevor Debardeleben <trevordebard@gmail.com>
2021-01-04 20:49:38 -0500

    Redirect /admin to Strapi application

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['localhost'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://secure-waters-64507.herokuapp.com/admin',
+        permanent: true,
+      },
+    ]
+  },
 }
